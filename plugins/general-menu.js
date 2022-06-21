1515e2c375f5910a19d67eba848bbc4f1ac4b6ea
Rii OFC ♡ <107350425+RiiOFC@users.noreply.github.com>
2022-06-21 17:54:56 +0700

    Update general-menu.js

--- plugins/general-menu.js
+++ plugins/general-menu.js
@@ -95,10 +95,10 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
    conn.sendButton(m.chat, `*${wish()}, ${name} 👋*`, text.trim(), await genProfile(conn, m), [['Speedtest', _p + 'ping'], ['Owner', _p + 'owner']], false, { quoted: fkon, contextInfo: { externalAdReply: { showAdAttribution: true,
 mediaType: 'VIDEO',
 mediaUrl: 'https://www.kibrispdr.org/dwn/7/yotsuba-nakano-wallpaper.jpg',
-title: 'Simple Bot Esm',
-body: 'By Papah-Chan',
+title: ' RiiBotあ',
+body: 'By Rii.',
 thumbnail: fs.readFileSync("./thumbnail.jpg"),
-sourceUrl: 'https://youtu.be/poD-7_U3jXk'
+sourceUrl: 'https://youtu.be/ASDH5JF7cho'
 }
   }
 })
